920bd0eebea01f0259302b423169cf076778e30d
NishanBhattarai327 <bhattarainishan327@gmail.com>
2021-07-05 11:00:26 +0545

    Add Html-webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	mode: 'development',
-	entry: './src/index.js',
+	entry: {
+		index: './src/index.js',
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		contentBase: './dist',
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Todos',
+		}),
+	],
 	output: {
-		filename: 'main.js',
+		filename: '[name].bundle.js',
 		path: path.resolve(__dirname, 'dist'),
+		clean: true,
 	},
 	module: {
 		rules: [
